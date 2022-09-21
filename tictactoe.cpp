2deb3bf4e36a1decc1c39128001fb36583238a3c
U-BSD\398256 <398256@SHS-DGDRCS2.beaverton.k12.or.us>
2022-09-21 14:35:40 -0700

    Start

---
+++ tictactoe.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ 
+
+}
+
+
+//Basic Idea for Tic Tac Toe:
+
+// create a 4x4 grid
+//  1  2  3
+//a
+//b
+//c
+
+//within a while game is playing = true
+  //let a user choose a spot
+  //check if valid (equals a blank space)
+  //if x's turn place an x otherwise place an o
+  //check for x wins/y wins/ties
+  //switch to other's turn
+
+//to check for x wins/ y wins (two functions: one for x checks, one for y)
+  //go through all spots:
+    //if winning spots are ocupied return (x or y) winner depending on which function is performing 
