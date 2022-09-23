4b3eae96ff0f4c6bf1149536eb6066ce984ce751
U-BSD\398256 <398256@SHS-DGDRCS2.beaverton.k12.or.us>
2022-09-23 14:27:27 -0700

    Message

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,17 +1,85 @@
 #include <iostream>
+#include <cctype>
+#include <cstring>
 
 using namespace std;
 
+//function declarations:
+void runGame();
+void printGrid(char board[4][7]);
+
 int main(){
+  //Grid setup:
+  char grid[4][7];
+  for(int y = 1; y <= 4; y++){
+    for(int x = 1; x <= 7; x++){
+      grid[y][x] = ' ';
+    }
+  }
+  grid[1][3] = '1';
+  grid[1][5] = '2';
+  grid[1][7] = '3';
+  grid[2][1] = 'a';
+  grid[3][1] = 'b';
+  grid[4][1] = 'c';
+ 
+  printGrid(grid);
+
  
 
+  runGame();
+
+}
+
+/*
+ *This function will run until someone wins
+ */
+void runGame(){
+  bool playing = true;
+  const char X_TURN = 'X';
+  const char Y_TURN = 'O';
+  char turn = 'X';
+  char input[3];
+  
+  while(playing){ 
+   cout << "Player " << turn << ", what's your move?" << endl; 
+   cin.get(input, 3);
+   if(input[0] >= 49 ||
+      input[0] <= 51){ //first letter typed is a number b/w 1-3
+    if(input[1] >= 97 ||
+       input[1] <= 99){ //second letter is a letter b/w a-c
+      cout << "IT WORKED" << endl;
+    }
+   }
+   if(turn == 'X'){
+     turn = 'O';
+   }else {
+     turn = 'X';
+   }
+   input //WORK HERE> Reset input please
+
+   
+  }
+}
+  
+
+/*
+ * This function will print the values of the grid to the console
+ */
+void printGrid(char board[4][7]){
+  for(int y = 1; y <= 4; y++){
+    for(int x = 1; x <= 7; x++){
+      cout << board[y][x];
+     }
+    cout << endl;
+  }
 }
 
 
 //Basic Idea for Tic Tac Toe:
 
 // create a 4x4 grid
-//  1  2  3
+//  123
 //a
 //b
 //c
